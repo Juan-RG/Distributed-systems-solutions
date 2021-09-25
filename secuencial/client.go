c4b413498e43a90a7b7989bc445d4cf78925b5c1
Juan <805001@unizar.es>
2021-09-25 23:37:18 +0200

    secuencial good

--- secuencial/client.go
+++ secuencial/client.go
@@ -10,15 +10,12 @@ package main
 
 import (
     "fmt"
-    "os"
     "net"
     "com"
+    "os"
 
-    "bytes"
-    "compress/gzip"
     "encoding/gob"
-    "io/ioutil"
-    "log"
+
 )
 
 func checkError(err error) {
@@ -28,66 +25,20 @@ func checkError(err error) {
 	}
 }
 
-//Funcion para pasar a [] bytes
-func EncodeToBytes(p interface{}) []byte {
-
-    buf := bytes.Buffer{}
-    enc := gob.NewEncoder(&buf)
-    err := enc.Encode(p)
-    if err != nil {
-        log.Fatal(err)
-    }
-    //devuelvo un struct como bytes
-    return buf.Bytes()
-}
-
-//funcion para comprimir
-func Compress(s []byte) []byte {
-
-    zipbuf := bytes.Buffer{}
-    zipped := gzip.NewWriter(&zipbuf)
-    zipped.Write(s)
-    zipped.Close()
-    //comprimo el array de bytes para enviar menos informacion
-    return zipbuf.Bytes()
-}
-
-//post return a RePLY
-func Decode(data []byte) com.Reply {
-
-    r := com.Reply{}
-    dec := gob.NewDecoder(bytes.NewReader(data))
-    err := dec.Decode(&r)
-    if err != nil {
-        log.Fatal(err)
-    }
-    //Devuelvo un reply decodificado de un array de bytes
-    return r
-}
-//function for descompress the data
-func Decompress(s []byte) []byte {
-
-    rdr, _ := gzip.NewReader(bytes.NewReader(s))
-    data, err := ioutil.ReadAll(rdr)
-    if err != nil {
-        log.Fatal(err)
-    }
-    rdr.Close()
-    //descomprimo un array de bytes comprimido
-    return data
-}
 
 func main(){
     endpoint := "127.0.0.1:30000"
     
-    var num1, num2 int
+    //var num1, num2 int
     //Pedimos el intervalo de numeros en el cual se buscaran los primos
-    fmt.Println("Introduce el numero 1:")
-    fmt.Scanln(&num1)
-    fmt.Println("Introduce el numero 2:")
-    fmt.Scanln(&num2)
+    //fmt.Println("Introduce el numero 1:")
+    //fmt.Scanln(&num1)
+    //fmt.Println("Introduce el numero 2:")
+    //fmt.Scanln(&num2)
 
-    interval := com.TPInterval{num1, num2}
+    //interval := com.TPInterval{num1, num2}
+
+    interval := com.TPInterval{1000, 70000}
 
     tcpAddr, err := net.ResolveTCPAddr("tcp", endpoint)
     checkError(err)
@@ -98,40 +49,15 @@ func main(){
     //modigicar el id para que no sea un hardcorde -> idea:Crear variable global que se vaya incrementando                  ToDo: hacer que el ID sea incrementar o aleatorio
     request := com.Request{0, interval}
 
+    encoder := gob.NewEncoder(conn)
+    decoder := gob.NewDecoder(conn)
 
-    //paso el struct a bytes
-    dataOut := EncodeToBytes(request)
-    //Lo comprimo para enviar menos bytes
-    dataOut = Compress(dataOut)
-    //Lo escribo en el socket
-    conn.Write(dataOut)
+    err = encoder.Encode(request)
+    checkError(err)
 
-    //espero la respuesta
-    //Buff of 1024  because TCP only contain up to 65495 bytes of payload
-    buf := make([]byte, 1024)
-    //mensaje completo
-    var allMsg []byte
-    //bytes recibidos
-    total := 0
-    for {
-        //leo del socket
-        n, err := conn.Read(buf)
-        if err != nil {
-            //Cambiar por una condicion de for el break no me gusta
-            break
-        }
-        //Acumulo los bytes totales recibidos
-        total += n
-        //forma de crear un array dinamico en goland
-        allMsg = append(allMsg, buf...)
-    }
+    var reply com.Reply
+    err = decoder.Decode(&reply)
+    checkError(err)
 
-    //Descomprimo los datos recibidos de 0 -> N(numero de bytes recibidos)
-    dataIn := Decompress(allMsg[0:total])
-    //println(dataIn)
-    //lo decodifico para obtener la respuesta
-    reply := Decode(dataIn)
-    
     fmt.Println(reply)
-
 }
--- secuencial/com/definitions.go
+++ secuencial/com/definitions.go
@@ -9,7 +9,6 @@
 */
 package com
 
-//import "time"
 
 type TPInterval struct {
     A int
